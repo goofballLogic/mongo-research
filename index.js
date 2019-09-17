ebb1e34131eb7b276b06c5548b41ccaaf7b2ea0a
Andrew Gibson <agibson@hublsoft.com>
2019-09-17 14:57:35 +0100

    prettier

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ const url = "mongodb://localhost:27017";
 const dbName = "cool_db";
 
 (async function() {
-
-    const client = new MongoClient(url);
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const stuff = db.collection("stuff");
-        const query = { "hello": { $regex: /^wor/ } };
-        const items = await stuff.find(query).toArray();
-        console.log(items);
-
-    } finally {
-
-        client.close();
-
-    }
-
-}());
+  const client = new MongoClient(url);
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const stuff = db.collection("stuff");
+    const query = { hello: { $regex: /^wor/ } };
+    const items = await stuff.find(query).toArray();
+    console.log(items);
+  } finally {
+    client.close();
+  }
+})();
